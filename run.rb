6ea38a9717ca85156ccfc3ca951d16b5d8054481
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-06-25 12:47:01 -0400

    real progress now

--- run.rb
+++ run.rb
@@ -4,19 +4,27 @@ require 'bundler/setup'
 Bundler.require
 
 opts = Trollop::options do
-  opt :all, "List All Tasks", :default => false
   opt :database, "Specify Task Database", :default => "#{ENV['HOME']}/.simple_todo.sqlite3"
+  opt :resetdb, "Clear out old database. Be careful!", :default => false
+  opt :action, "Action", :default => 'list'
 end
 
 #puts opts.inspect
 
-db = SQLite3::Database.new(opts[:database])
+# maybe delete db first
+File.delete(opts[:database]) if opts[:resetdb] && File.exist?(opts[:database])
+
+SQLite3::Database.new(opts[:database])
 ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => opts[:database])
 
 # define schema
+# id column gets set up automatically
 def initialize_database
   ActiveRecord::Base.connection.create_table(:tasks) do |t|
     t.column :content, :string, :limit => 256
+    t.column :created_at, :timestamp
+    t.column :updated_at, :timestamp
+    t.column :status, :string, :limit => 32, :default => 'active'
   end
 end
 
@@ -26,4 +34,6 @@ require 'task'
 # assume if this one table doesn't exist, need to make all tables
 initialize_database if !Task.table_exists?
 
-
+if opts[:action] == "list"
+  Task.print_all
+end
--- task.rb
+++ task.rb
@@ -1,2 +1,9 @@
 class Task < ActiveRecord::Base
+  class << self
+    def print_all
+      Task.all.each do |t|
+        puts "#{t.id}: #{t.content}"
+      end
+    end
+  end
 end
