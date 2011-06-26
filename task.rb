6effeb753cc8804cb15134c22cdb6581957d6e9d
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-06-26 16:15:19 -0400

    show 'X's next to completed ones if viewing all

--- task.rb
+++ task.rb
@@ -7,17 +7,26 @@ class Task < ActiveRecord::Base
     update_attribute(:status, 'active')
   end
 
-  def print
+  def print(include_status = false)
     id_space = Task.order('id DESC').limit(1).first.id.to_s.length
     print_id = id.to_s.rjust(id_space)
 
-    puts "#{print_id}: #{content}"
+    puts "#{status_string if include_status}#{print_id}: #{content}"
+  end
+  
+  def status_string
+    case status
+      when 'complete'
+        'X '
+      else
+        '  '
+      end
   end
 
   class << self
     def print_all
       Task.all.each do |t|
-        t.print
+        t.print(true)
       end
     end
 
