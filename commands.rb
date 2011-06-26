64064b287507120b20a45cb0a435b5fb513cad14
Robert Kaufman <rob.m.kaufman@gmail.com>
2011-06-26 16:05:14 -0400

    refactor

--- commands.rb
+++ commands.rb
@@ -1,32 +1,49 @@
 class Commands
   class << self
     def process(global_opts, cmd_opts, cmd, rest)
-      case cmd
+      @global_opts = global_opts
+      @cmd_opts = cmd_opts
+      @cmd = cmd
+      @rest = rest
+
+      case @cmd
         when 'list', 'l'
-          if cmd_opts[:all]
-            Task.print_all
-          else
-            Task.print_all_incomplete
-          end
+          list
         when 'add', 'a'
-          if t = Task.create(:content => ARGV.join(" "))
-            puts "new task created: #{t.inspect}"
-          else
-            puts "error saving task: #{t.inspect}"
-          end
+          add
         when 'complete', 'c'
-          id = ARGV.shift.to_i
-          t = Task.find(id)
-          if t.nil?
-            puts "no task with id #{id}"
-          else
-            if t.complete!
-              puts "task completed: #{t.inspect}"
-            else
-              puts "error completing task: #{t.inspect}"
-            end
-          end
+          complete
         end
+    end
+    
+    def list
+      if @cmd_opts[:all]
+        Task.print_all
+      else
+        Task.print_all_incomplete
+      end
+    end
+    
+    def add
+      if t = Task.create(:content => @rest.join(' '))
+        puts "new task created: #{t.inspect}"
+      else
+        puts "error saving task: #{t.inspect}"
+      end
+    end
+
+    def complete
+      id = @rest.shift.to_i
+      t = Task.find(id)
+      if t.nil?
+        puts "no task with id #{id}"
+      else
+        if t.complete!
+          puts "task completed: #{t.inspect}"
+        else
+          puts "error completing task: #{t.inspect}"
+        end
+      end
     end
   end
 end
